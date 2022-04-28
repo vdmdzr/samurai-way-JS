0f4e174b890cf62e37fadde83df4b58076b88da4
vdmdzr <vdmdzr@gmail.com>
2022-04-28 10:51:27 +0300

    51 lesson (Users API)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,34 +1,13 @@
 import React from "react";
 import './users.css'
+import * as axios from "axios";
+import userPhoto from '../../assets/images/png-clipart.png'
 
 export const Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoURL: 'https://avatars.mds.yandex.net/i?id=2a00000179fec733273e76907b06b21df92a-5132286-images-thumbs&n=13&exp=1',
-                followed: false,
-                fullName: 'Vadim',
-                status: 'Hi there!',
-                location: {city: 'Gomel', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                photoURL: 'https://pixelbox.ru/wp-content/uploads/2020/12/ava-vk-cats-92.jpg',
-                followed: false,
-                fullName: 'Vasya',
-                status: 'Good bye!',
-                location: {city: 'Kiev', country: 'Ukraine'}
-            },
-            {
-                id: 3,
-                photoURL: 'https://avatars.mds.yandex.net/i?id=e57b1daf86a74bb34443c824e884f634-5488376-images-thumbs&n=13&exp=1',
-                followed: true,
-                fullName: 'Valerchik',
-                status: 'im fine bro',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-        ])
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items)
+        })
     }
 
     return (
@@ -36,7 +15,7 @@ export const Users = (props) => {
           {props.users.map(u => <div key={u.id}>
               <span>
                   <div>
-                      <img className={'photoUser'} src={u.photoURL}/>
+                      <img className={'photoUser'} src={u.photos.small ? u.photos.small : userPhoto}/>
                   </div>
                   <div>
                       {u.followed
@@ -46,12 +25,12 @@ export const Users = (props) => {
               </span>
               <span>
                   <span>
-                      <div>{u.fullName}</div>
+                      <div>{u.name}</div>
                       <div>{u.status}</div>
                   </span>
                   <span>
-                      <div>{u.location.country}</div>
-                      <div>{u.location.city}</div>
+                      <div>{"u.location.country"}</div>
+                      <div>{"u.location.city"}</div>
                   </span>
               </span>
           </div>)}
