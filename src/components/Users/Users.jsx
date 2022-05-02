94025cd406e419405edc6fec564192c9473c097a
vdmdzr <vdmdzr@gmail.com>
2022-05-02 12:16:04 +0300

    54 lesson componentDidMount()

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,11 +4,11 @@ import * as axios from "axios";
 import userPhoto from '../../assets/images/png-clipart.png'
 
 export class Users extends React.Component {
-    constructor(props) {
-        super(props)
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items)
-            })
+
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
 
     render() {
