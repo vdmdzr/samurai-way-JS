56ce77b07f58b0c8e86a046a5faa15cb88bc18a2
vdmdzr <vdmdzr@gmail.com>
2022-06-17 22:45:28 +0300

    85 series (useEffect)

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,8 +1,12 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 export const ProfileStatusWithHooks = (props) => {
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+
+    useEffect(()=>{
+        setStatus(props.status)
+    },[])
 
     const activateEditMode = () => {
         setEditMode(true)
